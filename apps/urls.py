f456196cfad5f29576c7998ae7c75724d9650837
Benson Mogambi <mogambibenson2@gmail.com>
2024-11-18 14:56:50 +0300

    nothing new

--- apps/urls.py
+++ apps/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from apps import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('blog/', views.blog, name='blog'),
 
 
 ]
